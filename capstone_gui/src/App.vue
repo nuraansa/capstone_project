328248cef232f45ef42f0ad01ea36d0e6efd06a6
nuraansa <nuraansa@gmail.com>
2023-09-04 11:31:08 +0200

    folder structure

--- capstone_gui/src/App.vue
+++ capstone_gui/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
 </template>
 
+<script>
+import Navbar from "@/components/NavbarComp.vue"
+import Footer from "@/components/FooterComp.vue"
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
