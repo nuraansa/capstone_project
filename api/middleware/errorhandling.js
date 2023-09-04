1ee8b6c64c1f10a62087ca9005087bc098c61496
nuraansa <nuraansa@gmail.com>
2023-09-04 09:16:10 +0200

    update

--- api/middleware/errorhandling.js
+++ api/middleware/errorhandling.js
@@ -4,9 +4,10 @@ function errorHandling(err, req, res, next) {
         let status = err.status || 500
         res.json({
             status,
-            msg: "Oops an error occured, please try again later."
+            msg: "Oops a server error occured, please try again later."
         })
     }
     next()
 }
 module.exports = errorHandling
+ 
