7ab7664d66e9c4935fc525b8357f46e919011569
nuraansa <nuraansa@gmail.com>
2023-09-06 10:16:35 +0200

    update

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const app = express()
 const path = require('path')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const errorHandling = require('./middleware/errorhandling')
+const errorHandling = require('./middleware/errorhandling.js')
 const port = +process.env.PORT || 3000
 
 app.use((req, res, next)=>{
